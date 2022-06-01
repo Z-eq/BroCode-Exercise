5b1215e4c93d3de50e393c18311f3f53ca219a27
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-06-01 18:32:11 +0200

    renamed

--- src/main/java/learnwith/brocode/Randoms.java
+++ src/main/java/learnwith/brocode/Randoms.java
@@ -1,16 +1,16 @@
 package learnwith.brocode;
 import java.util.Random;
-public class Main {
+public class Randoms {
     public static void main(String[] args) {
 
         Random random = new Random();  // This is a created random class!
 
        // int x = random.nextInt();  // we can also bound a number to generate beetween in this case 0-33 : int x = random.nextInt(33);
-       // int x = random.nextInt(6)+1;
+        int x = random.nextInt(6)+1;
       //  double y = random.nextDouble();
-        boolean z = random.nextBoolean();
+     //  boolean z = random.nextBoolean();
 
-        System.out.println(z);
+        System.out.println(x);
 
 
     }
